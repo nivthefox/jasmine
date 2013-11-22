8346efd1e4c9f069bb25a0866f03d4d49fce8cd1
Kevin Kragenbrink <kevin@gaikai.com>
2013-11-21 16:49:34 -0800

    Adding log4js configuration object.

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 global.$ROOT_DIR = process.cwd();
 global.$SRC_DIR = $ROOT_DIR + (process.env.COVERAGE === '1' ? '/cov' : '/src');
 global.$CFG_DIR = $ROOT_DIR + '/cfg';
+global.$LOG_DIR = $ROOT_DIR + '/log';
