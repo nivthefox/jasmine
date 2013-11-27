360f122067dbf0ae96648539cd51f3895ab77176
Kevin Kragenbrink <kevin@gaikai.com>
2013-11-26 18:39:45 -0800

    Initial Interpreter.

---
+++ src/Interpreter.js
@@ -0,0 +1,53 @@
+/**
+ * __          __   _ _   _                  _
+ * \ \        / /  (_) | | |                | |
+ *  \ \  /\  / / __ _| |_| |__    _ __   ___| |_
+ *   \ \/  \/ / '__| | __| '_ \  | '_ \ / _ \ __|
+ *    \  /\  /| |  | | |_| | | |_| | | |  __/ |_
+ *     \/  \/ |_|  |_|\__|_| |_(_)_| |_|\___|\__|
+ *
+ * Copyright (C) 2013 Kevin Kragenbrink <kevin@writh.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var commands = new EventEmitter;
+commands.emit = process.emit;
+
+process.on('session.received.data', function (session, data) {
+    var cmd = data.substr(0, data.indexOf(' '));
+        data = data.substr(cmd.length+1);
+    commands.emit(cmd, session, data);
+});
+
+var Interpreter = function (command, callback) {
+
+    this.remove = function () {
+        commands.removeListener(command, callback);
+    };
+
+    // @constructor
+    {
+        commands.on(command, callback);
+    }
+};
+
+module.exports = Interpreter;
