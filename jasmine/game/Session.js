d9218592f4d9e3d17b9927904443a8dc385439d0
Kevin Kragenbrink <kevin@writh.net>
2015-08-16 02:50:23 -0700

    Added at_become behaviour.

--- jasmine/game/Session.js
+++ jasmine/game/Session.js
@@ -1,17 +1,33 @@
+'use strict';
+
 const JasmineSession = require('jasmine/Session');
 
+// LOGIN COMMANDS
 const CreateCommand = require('game/commands/login/Create');
 const LoginCommand = require('game/commands/login/Login');
 const QuitCommand = require('game/commands/login/Quit');
 
+// PLAYER COMMANDS
+
 class Session extends JasmineSession {
-    constructor (socket) {
-        super(socket);
+    constructor () {
+        super();
 
         this.addCommand(CreateCommand);
         this.addCommand(LoginCommand);
         this.addCommand(QuitCommand);
     }
+
+    at_become (obj) {
+        // Remove login commands.
+        this.removeCommand(CreateCommand);
+        this.removeCommand(LoginCommand);
+
+        // Add player commands.
+    }
+
+    at_command_not_found () {}
+    at_connect () {}
 }
 
 module.exports = Session;
